e760a21df788eebc46343c720957a60908efcc99
Tim Lucas <t@toolmantim.com>
2018-08-07 20:03:40 +1000

    Style fixes

--- test/commits.test.js
+++ test/commits.test.js
@@ -1,16 +1,14 @@
-const commits = require('../lib/commits');
+const commits = require('../lib/commits')
 
-
-const mockCommit = message => ({ commit: { message }});
-
+const mockCommit = message => ({ commit: { message } })
 
 describe('extractPullRequestNumber', () => {
   it('extracts from merge commit message', () => {
     expect(
       commits.extractPullRequestNumber(mockCommit(
-        'Merge pull request #37 from JasonEtco/patch-1'))
-      ).toEqual("37");
-  });
+        'Merge pull request #37 from JasonEtco/patch-1')
+      )).toEqual('37')
+  })
 
   it('extracts from squash & merge commit message', () => {
     expect(
@@ -21,7 +19,7 @@ Fixed conditional card state on 1st render (#1537)
 * Fixed conditional card state on 1st render
 
 * Moved method to correct location
-        `.trim()))
-      ).toEqual("1537");
-  });
-});
+        `.trim())
+      )).toEqual('1537')
+  })
+})
