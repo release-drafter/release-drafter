7195c4406c14898225eb9dd9895888e78e468bd9
Paulus Schoutsen <balloob@gmail.com>
2018-08-07 10:02:07 +0200

    Add tests for extracting commit numbers

---
+++ test/commits.test.js
@@ -0,0 +1,21 @@
+const commits = require('../lib/commits');
+
+
+const mockCommit = message => ({ commit: { message }});
+
+
+describe('extractPullRequestNumber', () => {
+  it('extracts from merge commit message', () => {
+    expect(
+      commits.extractPullRequestNumber(mockCommit(
+        'Merge pull request #37 from JasonEtco/patch-1'))
+      ).toEqual("37");
+  });
+
+  it('extracts from squash & merge commit message', () => {
+    expect(
+      commits.extractPullRequestNumber(mockCommit(
+        'Fixed conditional card state on 1st render (#1537)'))
+      ).toEqual("1537");
+  });
+});
