c2e8b1b77f3cd39e8afde61c64356ee493e992a9
Dan Chowdhury <dan.chowdhury@babylonhealth.com>
2020-05-13 02:00:47 +0100

    Add support for excluded labels

--- test/index.test.js
+++ test/index.test.js
@@ -1890,7 +1890,7 @@ Previous tag: ''
         response.data.repository.ref.target.history.nodes[1].associatedPullRequests.nodes[0].labels.nodes = [
           { name: 'MIN' }
         ]
-        response.data.repository.ref.target.history.nodes[1].associatedPullRequests.nodes[0].labels.nodes = [
+        response.data.repository.ref.target.history.nodes[2].associatedPullRequests.nodes[0].labels.nodes = [
           { name: 'MAJ' }
         ]
 
@@ -1915,6 +1915,55 @@ Previous tag: ''
                 draft: true,
                 name: 'v3.0.0',
                 tag_name: 'v3.0.0'
+              })
+              return true
+            }
+          )
+          .reply(200, require('./fixtures/release'))
+
+        await probot.receive({
+          name: 'push',
+          payload: require('./fixtures/push')
+        })
+
+        expect.assertions(1)
+      })
+      it('exclusions dont impact version resolver', async () => {
+        getConfigMock('config-with-custom-version-resolver.yml')
+
+        const response = require('./fixtures/__generated__/graphql-commits-merge-commit.json')
+        response.data.repository.ref.target.history.nodes[0].associatedPullRequests.nodes[0].labels.nodes = [
+          { name: 'PAT' }
+        ]
+        response.data.repository.ref.target.history.nodes[1].associatedPullRequests.nodes[0].labels.nodes = [
+          { name: 'MIN' }
+        ]
+        response.data.repository.ref.target.history.nodes[2].associatedPullRequests.nodes[0].labels.nodes = [
+          { name: 'MAJ' },
+          { name: 'skip-changelog' }
+        ]
+
+        nock('https://api.github.com')
+          .get(
+            '/repos/toolmantim/release-drafter-test-project/releases?per_page=100'
+          )
+          .reply(200, [require('./fixtures/release')])
+
+        nock('https://api.github.com')
+          .post('/graphql', body =>
+            body.query.includes('query findCommitsWithAssociatedPullRequests')
+          )
+          .reply(200, response)
+
+        nock('https://api.github.com')
+          .post(
+            '/repos/toolmantim/release-drafter-test-project/releases',
+            body => {
+              expect(body).toMatchObject({
+                body: `dummy`,
+                draft: true,
+                name: 'v2.1.0',
+                tag_name: 'v2.1.0'
               })
               return true
             }
