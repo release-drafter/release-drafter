2549de2cffe1a666cb14e505f4240126734c6ed1
Hadrian Tang <hadrianwttang@outlook.com>
2020-08-01 16:22:09 +0800

    Create releases.test.js

---
+++ test/releases.test.js
@@ -0,0 +1,24 @@
+const { generateChangeLog } = require('../lib/releases')
+const each = require('jest-each').default
+const { DEFAULT_CONFIG } = require('../lib/default-config')
+
+const config = {
+  ...DEFAULT_CONFIG,
+  template: '$CHANGES',
+  references: ['master'],
+}
+
+describe('releases', () => {
+  describe('generateChangeLog', () => {
+    it('does not escape normal titles', () => {
+      const pullRequests = [
+        { title: 'A1', number: 1, body: 'A1 body', url: 'https://github.com', labels:{ nodes:[{ name: 'bug'}] } },
+        { title: 'B2', number: 2, body: 'B2 body', url: 'https://github.com', labels:{ nodes:[{ name: 'feature' }] } },
+        { title: 'C3', number: 3, body: 'C3 body', url: 'https://github.com', labels:{ nodes:[{ name: 'bug'}] } },
+      ]
+      const changelog = generateChangeLog(pullRequests, config)
+  
+      expect(changelog).toEqual('11.0.0') // Getting output...
+    })
+  })
+})
