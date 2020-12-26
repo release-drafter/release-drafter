1c5da43737a3918af5c9151d25e3aa2561112804
Joseph Petersen <josephp90@gmail.com>
2020-12-26 08:04:58 +0100

    context.config now returns a simpler object

--- test/helpers/config-mock.js
+++ test/helpers/config-mock.js
@@ -6,26 +6,7 @@ function configFixture(
   fileName = 'config.yml',
   repoFileName = 'release-drafter.yml'
 ) {
-  return {
-    type: 'file',
-    encoding: 'base64',
-    size: 5362,
-    name: repoFileName,
-    path: `.github/${repoFileName}`,
-    content: encodeContent(
-      fs.readFileSync(`${__dirname}/../fixtures/config/${fileName}`)
-    ),
-    sha: '3d21ec53a331a6f037a91c368710b99387d012c1',
-    url: `https://api.github.com/repos/octokit/octokit.rb/contents/.github/${repoFileName}`,
-    git_url: `https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1`,
-    html_url: `https://github.com/octokit/octokit.rb/blob/master/.github/${repoFileName}`,
-    download_url: `https://raw.githubusercontent.com/octokit/octokit.rb/master/.github/${repoFileName}`,
-    _links: {
-      git: `https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1`,
-      self: `https://api.github.com/repos/octokit/octokit.rb/contents/.github/${repoFileName}`,
-      html: `https://github.com/octokit/octokit.rb/blob/master/.github/${repoFileName}`,
-    },
-  }
+  return fs.readFileSync(`${__dirname}/../fixtures/config/${fileName}`)
 }
 
 module.exports = function getConfigMock(
