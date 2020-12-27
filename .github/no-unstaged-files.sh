e73db15364de32efea3bd9663a5a7c04554d7d23
Joseph Petersen <josephp90@gmail.com>
2020-12-27 10:30:54 +0100

    fix yarn lint / prettier

--- .github/no-unstaged-files.sh
+++ .github/no-unstaged-files.sh
@@ -2,6 +2,6 @@
 
 if [[ "$(git status --porcelain)" != "" ]]; then
   git status
-  echo "::error::💥 Unstaged changes detected. Locally try running: yarn prettier && yarn build"
+  echo "::error::💥 Unstaged changes detected. Locally try running: yarn prettier && yarn lint --fix && yarn build"
   exit 1
 fi
