900a498b4505fa706670901937ac0b9c04a29b10
Joseph Petersen <josephp90@gmail.com>
2021-02-11 07:37:58 +0100

    build

--- action.js
+++ action.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core')
+const { run } = require('@probot/adapter-github-actions')
 const app = require('./index')
-
-const { run } = require('@probot/github-action')
 
 run(app).catch((err) => {
   core.setFailed(`💥 Release drafter failed with error: ${err.message}`)
