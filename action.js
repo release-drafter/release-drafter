6f8e1580788928f08844fba49048d1e85327c31a
Joseph Petersen <josephp90@gmail.com>
2021-02-11 07:40:31 +0100

    use probot github adapter (#782)

--- action.js
+++ action.js
@@ -1,42 +1,7 @@
-const { createProbot } = require('probot')
 const core = require('@actions/core')
+const { run } = require('@probot/adapter-github-actions')
 const app = require('./index')
 
-run().catch((err) => {
+run(app).catch((err) => {
   core.setFailed(`💥 Release drafter failed with error: ${err.message}`)
 })
-
-async function run() {
-  if (!process.env.GITHUB_TOKEN) {
-    throw new Error(
-      'env.GITHUB_TOKEN must be set, see https://github.com/probot/example-github-action#usage'
-    )
-  }
-
-  const envVariablesMissing = [
-    'GITHUB_RUN_ID',
-    'GITHUB_EVENT_NAME',
-    'GITHUB_EVENT_PATH',
-  ].filter((name) => !process.env[name])
-
-  if (envVariablesMissing.length) {
-    const missingEnvs = envVariablesMissing.join(', ')
-    throw new Error(
-      `GitHub Action default environment variables missing: ${missingEnvs}. See https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables#default-environment-variables`
-    )
-  }
-
-  const probot = createProbot({
-    overrides: {
-      githubToken: process.env.GITHUB_TOKEN,
-    },
-  })
-
-  await probot.load(app)
-
-  return probot.receive({
-    id: process.env.GITHUB_RUN_ID,
-    name: process.env.GITHUB_EVENT_NAME,
-    payload: require(process.env.GITHUB_EVENT_PATH),
-  })
-}
