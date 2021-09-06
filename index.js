8c2bf2805c06897c03e7f5034b424598defb09fb
Daniel Weber <danielcweber@users.noreply.github.com>
2021-09-06 17:42:35 +0200

    Use commitish from action inputs. Fixes #824. (#865)
    Co-authored-by: Mariusz Kogut <mk@hanmdade-systems.de>

--- index.js
+++ index.js
@@ -132,6 +132,7 @@ module.exports = (app, { getRouter }) => {
       tag,
       name,
       disableReleaser,
+      commitish,
     } = getInput()
 
     const config = await getConfig({
@@ -182,6 +183,7 @@ module.exports = (app, { getRouter }) => {
       name,
       isPreRelease,
       shouldDraft,
+      commitish,
     })
 
     let createOrUpdateReleaseResponse
@@ -221,6 +223,7 @@ function getInput({ config } = {}) {
         core.getInput('disable-releaser').toLowerCase() === 'true',
       disableAutolabeler:
         core.getInput('disable-autolabeler').toLowerCase() === 'true',
+      commitish: core.getInput('commitish') || undefined,
     }
   }
 
