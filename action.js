c341d3d6c8862d267565964f6e0263f9aeed6a61
Joseph Petersen <josephp90@gmail.com>
2022-01-13 04:01:29 +0100

    fix server (#1010)

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 const { run } = require('@probot/adapter-github-actions')
-const { releaseDrafter } = require('./index')
+const releaseDrafter = require('./index')
 
 run(releaseDrafter).catch((error) => {
   core.setFailed(`💥 Release drafter failed with error: ${error.message}`)
