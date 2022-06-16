af6d765c6b44df86de6c21ff2b12bff64737f7e0
dreinon <danielreinon@outlook.com>
2022-06-16 00:05:27 +0000

    test: add closing issues to query fixtures

--- bin/generate-fixtures.js
+++ bin/generate-fixtures.js
@@ -86,6 +86,9 @@ for (const repo of repos) {
         withPullRequestURL: true,
         withBaseRefName: true,
         withHeadRefName: true,
+        withPullRequestClosingIssues: true,
+        withClosingIssueBody: true,
+        withClosingIssueURL: true,
       },
     })
   )
