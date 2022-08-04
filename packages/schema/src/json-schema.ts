498a6d8f5965c69982eb616315d9967c894ef59d
Joseph Petersen <me@jetersen.dev>
2022-08-04 02:38:02 +0200

    use zod over joi
    this alllows for easier transformation of string to regex!

--- packages/schema/src/json-schema.ts
+++ packages/schema/src/json-schema.ts
@@ -1,24 +1,23 @@
-import { convert } from '@koa-lite/joi-schema'
+import { zodToJsonSchema } from 'zod-to-json-schema'
 import { schema } from '@release-drafter/core'
 
 function convertSchema() {
-	const converted = convert(schema())
+	const converted = zodToJsonSchema(schema(), 'ReleaseDrafter')
 	// template is only required after deep merged, should not be required in the JSON schema
 	// we should also remove the required field in case nothing remains after the filtering to keep draft04 compatibility
-	const requiredField = converted.required.filter(
-		(item: string) => item !== 'template',
-	)
-	if (requiredField.length > 0) {
-		converted.required = requiredField
-	} else {
-		delete converted.required
-	}
+	// const requiredField = converted.required.filter(
+	// 	(item: string) => item !== 'template',
+	// )
+	// if (requiredField.length > 0) {
+	// 	converted.required = requiredField
+	// } else {
+	// 	delete converted.required
+	// }
 	return converted
 }
 
 export const jsonSchema = {
 	title: 'JSON schema for Release Drafter yaml files',
-	id: 'https://raw.githubusercontent.com/release-drafter/release-drafter/master/schema.json',
-	$schema: 'http://json-schema.org/draft-04/schema',
+	$id: 'https://raw.githubusercontent.com/release-drafter/release-drafter/master/schema.json',
 	...convertSchema(),
 }
--- packages/schema/src/main.ts
+++ packages/schema/src/main.ts
@@ -1,9 +1,12 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import url from 'node:url'
+import { gitRootSync } from '@antongolub/git-root'
 import { jsonSchema } from './json-schema.js'
 
 const directory = path.dirname(url.fileURLToPath(import.meta.url))
-const schemaPath = path.join(directory, '../../..', 'schema.json')
+const root = gitRootSync(directory) ?? ''
+
+const schemaPath = path.join(root.toString(), 'schema.json')
 
 fs.writeFileSync(schemaPath, `${JSON.stringify(jsonSchema, undefined, '\t')}\n`)
