1015a316aba9a8112a262048c3a7db590a9c1d17
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2025-12-29 17:45:18 +0000

    feat: implement testing utils and firt test

--- __fixtures__/core.ts
+++ __fixtures__/core.ts
@@ -4,6 +4,12 @@ import { jest } from '@jest/globals'
 export const debug = jest.fn<typeof core.debug>()
 export const error = jest.fn<typeof core.error>()
 export const info = jest.fn<typeof core.info>()
+
+// When debugging tests, you can uncomment these lines to see actual console output.
+// export const debug = jest.fn<typeof core.debug>(console.debug)
+// export const error = jest.fn<typeof core.error>(console.error)
+// export const info = jest.fn<typeof core.info>(console.info)
+
 export const getInput = jest.fn<typeof core.getInput>()
 export const setOutput = jest.fn<typeof core.setOutput>()
 export const setFailed = jest.fn<typeof core.setFailed>()
