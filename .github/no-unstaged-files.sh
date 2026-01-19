dfc1c4d8f37bee5f72376977d23d76b6263deefd
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-01-19 16:14:03 +0100

    feat: add diff of unstaged files

--- .github/no-unstaged-files.sh
+++ .github/no-unstaged-files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ "$(git status --porcelain)" != "" ]]; then
-  git status
+  git diff
+  git status -u all
   echo "::error::💥 Unstaged changes detected. Locally try running: yarn prettier && yarn lint --fix && yarn build"
   exit 1
 fi
