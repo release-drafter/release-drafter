cb5b847b1ea1d9d1ea96d1ec5267043f38bec486
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-01-19 15:20:37 +0100

    build: remove ncc in favor of rollup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.js': (filenames) => `eslint --fix ${filenames.join(' ')}`,
   '*.{md,json,yml,yaml}': (filenames) =>
     `prettier --write ${filenames.join(' ')}`,
-  'action.js|lib/**/*.js': () => 'ncc build action.js --target es2021',
+  'action.js|lib/**/*.js': () => 'npx rollup --config rollup.config.js',
 }
---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+// See: https://rollupjs.org/introduction/
+
+const commonjs = require('@rollup/plugin-commonjs')
+const nodeResolve = require('@rollup/plugin-node-resolve')
+const json = require('@rollup/plugin-json')
+
+module.exports = {
+  input: 'index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'commonjs',
+  },
+  plugins: [json(), nodeResolve({ preferBuiltins: true }), commonjs()],
+}
