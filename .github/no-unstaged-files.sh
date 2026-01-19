a38b31291bb6ceb8d1139f6fd70e98c3a94f24a4
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-01-19 17:30:17 +0100

    fix: make git touch the files at least once before checking for diffs

--- .github/no-unstaged-files.sh
+++ .github/no-unstaged-files.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git diff
+# The following makes git "touch" the files at leas one,
+# and applies the contents of .gitattributes the same way
+# a local developer would.
+# This helps alleviate line-endigs inconsistencies our tools
+# and dependencies might produce.
+git add .
+git reset
+
 if [[ "$(git status --porcelain)" != "" ]]; then
   git status -u all
   echo "::error::💥 Unstaged changes detected. Locally try running: yarn prettier && yarn lint --fix && yarn build"
