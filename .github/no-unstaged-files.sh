6db134d15f3909ccc9eefd369f02bd1e9cffdf97
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-01-22 10:45:40 +0100

    ci: add diff for debugging unstaged files

--- .github/no-unstaged-files.sh
+++ .github/no-unstaged-files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git diff
+
 if [[ "$(git status --porcelain)" != "" ]]; then
   git status
   echo "::error::💥 Unstaged changes detected. Locally try running: npm run prettier && npm run lint --fix && npm run build"
