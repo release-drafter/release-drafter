f38704cb3f99130e117fe08102182be9341cc9eb
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-02-06 15:44:11 +0100

    test: add an 2e2 test

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -162,5 +162,46 @@ describe('release-drafter', () => {
         expect(core.setFailed).not.toHaveBeenCalled()
       })
     })
+
+    describe('with past releases', () => {
+      it('creates a new draft listing the changes', async () => {
+        await mockContext('push')
+        mocks.config.mockReturnValue('config')
+
+        const gqlScope = mockGraphqlQuery({
+          payload: 'graphql-commits-merge-commit'
+        })
+        const scope = nockGetAndPostReleases({
+          fetchedReleases: ['release-2', 'release', 'release-3']
+        })
+
+        await runDrafter()
+
+        expect(mocks.postReleaseBody.mock.lastCall).toMatchInlineSnapshot(`
+          [
+            {
+              "body": "# What's Changed
+
+          * Add documentation (#5) @TimonVS
+          * Update dependencies (#4) @TimonVS
+          * Bug fixes (#3) @TimonVS
+          * Add big feature (#2) @TimonVS
+          * 👽 Add alien technology (#1) @TimonVS
+          ",
+              "draft": true,
+              "make_latest": "true",
+              "name": "",
+              "prerelease": false,
+              "tag_name": "",
+              "target_commitish": "refs/heads/master",
+            },
+          ]
+        `)
+
+        expect(scope.isDone()).toBe(true) // should call the mocked endpoints
+        expect(gqlScope.isDone()).toBe(true) // should call the mocked endpoints
+        expect(core.setFailed).not.toHaveBeenCalled()
+      })
+    })
   })
 })
