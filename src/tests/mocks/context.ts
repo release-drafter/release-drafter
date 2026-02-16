1fd0a45ec4b3869ffc625b09f223537a6fd075b8
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-02-16 14:07:54 +0100

    chore: remove @github/local-action

--- src/tests/mocks/context.ts
+++ src/tests/mocks/context.ts
@@ -1,12 +1,11 @@
 import { existsSync, readFileSync } from 'fs'
 import path from 'path'
-import dotenv from 'dotenv'
 import type { GithubActionEnvironment } from 'src/types'
 
-import type { WebhookPayload } from '@actions/github/lib/interfaces'
 import { expect, vi } from 'vitest'
 
-import github from '@actions/github'
+import * as github from '@actions/github'
+import { WebhookPayload } from 'node_modules/@actions/github/lib/interfaces'
 
 type AllowedPayload = 'push' | 'push-non-master-branch' | 'push-tag'
 
@@ -19,9 +18,6 @@ const getEventPayloadPath = (type: AllowedPayload) => {
   return path.join(baseDir, `${type}.json`)
 }
 
-const getExampleEnvPath = () =>
-  path.join(path.dirname(import.meta.filename), '../../../', '.env.example')
-
 /**
  * Mocking GitHub Action environment variables for testing.
  *
@@ -31,23 +27,61 @@ const getExampleEnvPath = () =>
  */
 export const mockContext = async (desiredPayload: AllowedPayload) => {
   let payload: WebhookPayload
-  let defaultEnv: GithubActionEnvironment
+  const defaultEnv: GithubActionEnvironment = {
+    CI: 'true',
+    GITHUB_ACTION: 'release-drafter',
+    GITHUB_ACTION_PATH: '',
+    GITHUB_ACTION_REPOSITORY: 'toolmantim/release-drafter-test-project',
+    GITHUB_ACTIONS: 'false',
+    GITHUB_ACTOR: 'toolmantim',
+    GITHUB_ACTOR_ID: '153',
+    GITHUB_API_URL: 'https://api.github.com',
+    GITHUB_BASE_REF: 'master',
+    GITHUB_ENV: '',
+    GITHUB_EVENT_NAME: 'push',
+    GITHUB_EVENT_PATH: '__fixtures__/events/push.json',
+    GITHUB_GRAPHQL_URL: 'https://api.github.com/graphql',
+    GITHUB_HEAD_REF: '',
+    GITHUB_JOB: 'release_drafter_job',
+    GITHUB_OUTPUT: '',
+    GITHUB_PATH: '',
+    GITHUB_REF: 'refs/heads/master',
+    GITHUB_REF_NAME: 'master',
+    GITHUB_REF_PROTECTED: 'true',
+    GITHUB_REF_TYPE: 'branch',
+    GITHUB_REPOSITORY: 'toolmantim/release-drafter-test-project',
+    GITHUB_REPOSITORY_ID: '133810100',
+    GITHUB_REPOSITORY_OWNER: 'toolmantim',
+    GITHUB_REPOSITORY_OWNER_ID: '153',
+    GITHUB_RETENTION_DAYS: '',
+    GITHUB_RUN_ATTEMPT: '1',
+    GITHUB_RUN_ID: '1658821493',
+    GITHUB_RUN_NUMBER: '1',
+    GITHUB_SERVER_URL: 'https://github.com',
+    GITHUB_SHA: '1496a1f82f32f240f7cbe1a42eb0b0c7a06a5093',
+    GITHUB_STEP_SUMMARY: '',
+    GITHUB_TRIGGERING_ACTOR: 'toolmantim',
+    GITHUB_WORKFLOW: 'Release Drafter Workflow',
+    GITHUB_WORKFLOW_REF:
+      'toolmantim/release-drafter-test-project/.github/workflows/my-workflow.yml@refs/heads/my_branch',
+    GITHUB_WORKFLOW_SHA: 'aa80724a5d394e0cadaeb0488c488fe21922d6ff',
+    GITHUB_WORKSPACE: '.',
+    RUNNER_ARCH: '',
+    RUNNER_DEBUG: '1',
+    RUNNER_NAME: '',
+    RUNNER_OS: 'Linux',
+    RUNNER_TEMP: '',
+    RUNNER_TOOL_CACHE: '',
+    ACTIONS_STEP_DEBUG: 'true',
+    INPUT_MILLISECONDS: '2400'
+  }
 
   const pathToPayload = getEventPayloadPath(desiredPayload)
-  const pathToDefaultEnv = getExampleEnvPath()
 
   if (existsSync(pathToPayload)) {
     payload = JSON.parse(readFileSync(pathToPayload, { encoding: 'utf8' }))
   } else {
     throw new Error('Event payload path does not exist')
-  }
-
-  if (existsSync(pathToDefaultEnv)) {
-    defaultEnv = dotenv.parse(
-      readFileSync(pathToDefaultEnv, { encoding: 'utf8' })
-    )
-  } else {
-    throw new Error('Default env path does not exist')
   }
 
   /**
@@ -84,8 +118,23 @@ export const mockContext = async (desiredPayload: AllowedPayload) => {
       vi.stubEnv(key, value)
     })
 
-  // @ts-expect-error - @actions/github has side effects on import that need to be re-initialized
-  github.context = new (await import('@actions/github/lib/context')).Context() // reads from env
+  // Context class is not explicitely exported by @actions/github
+  const newContext = new (
+    await import('node_modules/@actions/github/lib/context')
+  ).Context()
+
+  // Hack to change github.context despite being read-only - @actions/github has side effects on import that need to be re-initialized
+  Object.keys(github.context).forEach((contextKey) => {
+    const k = contextKey as keyof typeof github.context
+    const newValue = newContext[k]
+
+    if (k in github.context) {
+      // @ts-expect-error - please let me do my thing typescript thank you
+      github.context[k] = newValue
+    } else {
+      delete github.context[k]
+    }
+  })
 
   // Verify the context has been set up correctly
   const dynamicContext = (await import('@actions/github')).context
