5f33818ff0beaa1df7ebfd3c6b3b39958d95644b
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2026-03-07 17:30:05 +0100

    feat: add include-pre-releases to action inputs

--- index.js
+++ index.js
@@ -248,6 +248,10 @@ function getInput() {
         ? core.getInput('prerelease').toLowerCase() === 'true'
         : undefined,
     preReleaseIdentifier: core.getInput('prerelease-identifier') || undefined,
+    includePreReleases:
+      core.getInput('include-pre-releases') !== ''
+        ? core.getInput('include-pre-releases').toLowerCase() === 'true'
+        : undefined,
     latest: core.getInput('latest')?.toLowerCase() || undefined,
     commitsSince: core.getInput('initial-commits-since') || undefined,
   }
@@ -288,6 +292,10 @@ function updateConfigFromInput(config, input) {
 
   if (input.commitsSince) {
     config['initial-commits-since'] = input.commitsSince
+  }
+
+  if (input.includePreReleases !== undefined) {
+    config['include-pre-releases'] = input.includePreReleases
   }
 }
 
