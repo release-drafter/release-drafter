0ff2ccbcf9a4f746db5ae8eea08050e7a1d48f81
Joseph Petersen <me@jetersen.dev>
2026-08-22 20:13:23 +0200

    test(check-pr): update validation failure assertion

--- src/tests/check-pr/check-pr.e2e.test.ts
+++ src/tests/check-pr/check-pr.e2e.test.ts
@@ -24,7 +24,7 @@ describe('check-pr e2e', () => {
     await runCheckPr()
 
     expect(mocks.core.setFailed).toHaveBeenCalledWith(
-      expect.stringContaining('does not match'),
+      'No configured changelog or version-resolver category matches the title or labels of pull request #1475.',
     )
     expect(mocks.core.setOutput).not.toHaveBeenCalled()
     expect(mocks.postPrLabelsBody).not.toHaveBeenCalled()
